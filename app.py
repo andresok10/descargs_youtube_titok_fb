f15d98aeb15f0b97f8ffc85589cea6c63c6f567e
andresok10 <gdave0930@gmail.com>
2024-10-31 23:33:31 -0500

    ok

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def serve_file(filename):
     return send_from_directory(DOWNLOAD_PATH, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
