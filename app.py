df04be1ceee658d04c3b175c092b5531124c9ae3
andresok10 <gdave0930@gmail.com>
2024-11-02 06:15:20 -0500

    ok

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
+import yt_dlp
 from flask import Flask, render_template, request, send_from_directory
 import os
-import yt_dlp
 from moviepy.editor import AudioFileClip
 os.system("pip install --upgrade pip")
 app = Flask(__name__)
-# Carpeta donde se guardarán las descargas
 DOWNLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'downloads')
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
@@ -16,27 +15,36 @@ def index():
 def download():
     url = request.form['url']
     format_type = request.form.get('format', 'video')
-    # Configuración de yt-dlp
+    
+    # Configuración de yt-dlp con archivo de cookies
     ydl_opts = {
         'format': 'best',
-        'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s')
+        'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
+        'cookiefile': 'ruta/a/cookies.txt'  # Reemplaza con la ruta de tu archivo de cookies
     }
-    # Descargar el video
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(url, download=True)
-        video_title = result['title']
 
-    if format_type == 'audio':
-        video_file = os.path.join(DOWNLOAD_FOLDER, f"{video_title}.mp4")
-        audio_file = os.path.join(DOWNLOAD_FOLDER, f"{video_title}.mp3")
+    try:
+        # Descargar el video
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            result = ydl.extract_info(url, download=True)
+            video_title = result['title']
 
-        # Convertir el video a audio usando moviepy
-        clip = AudioFileClip(video_file)
-        clip.write_audiofile(audio_file)
-        clip.close()
-        # Opcionalmente, puedes eliminar el archivo de video si no lo necesitas
-        os.remove(video_file)
-    return f'Download completed! <a href="/downloads/{video_title}.mp3">Download audio</a>'
+        if format_type == 'audio':
+            video_file = os.path.join(DOWNLOAD_FOLDER, f"{video_title}.mp4")
+            audio_file = os.path.join(DOWNLOAD_FOLDER, f"{video_title}.mp3")
+
+            # Convertir el video a audio usando moviepy
+            clip = AudioFileClip(video_file)
+            clip.write_audiofile(audio_file)
+            clip.close()
+
+            # Eliminar el archivo de video si no es necesario
+            os.remove(video_file)
+
+        return f'Download completed! <a href="/downloads/{video_title}.mp3">Download audio</a>'
+
+    except Exception as e:
+        return f"Error: {e}", 500
 
 @app.route('/downloads/<filename>')
 def download_file(filename):
